455af0ce5005ac1abd281378d0e4cb4aac7c145c
lswapnil <lswapnil@in-lswapnil.local>
2018-10-10 17:16:24 +0530

    Just make a statement in starting of use of script

--- copyingArray.js
+++ copyingArray.js
@@ -1,3 +1,4 @@
+//this script will help user to make copy with different refference //
 const copyArray = function (array){
   let copiedArray = [];
   for(let elementsIndex=0; elementsIndex < array.length; elementsIndex++){
